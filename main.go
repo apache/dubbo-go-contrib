d57ef6a4d60e86ed093d8ad423721365c84a9678
Albumen Kevin <jhq0812@gmail.com>
2023-06-08 17:53:21 +0800

    fix go mod a

--- main.go
+++ main.go
@@ -13,6 +13,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-module github.com/apache/dubbo-go-contrib
+//
 
-go 1.15
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
